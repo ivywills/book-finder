3d936091528bdab92db70545c8898e6db8520a52
Ivy Wills <ivy.wills@mail.utoronto.ca>
2025-04-27 13:24:19 -0400

    feat: add ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5157675419011624"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
