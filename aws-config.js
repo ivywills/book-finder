d36b3e15d02a05df60f8867f6a5de3c5a651e115
Ivy Wills <ivy.wills@mail.utoronto.ca>
2025-05-28 20:03:21 -0400

    fix: remove unneeded code

--- aws-config.js
+++
@@ -1,9 +0,0 @@
-import AWS from 'aws-sdk';
-
-AWS.config.update({
-  accessKeyId: '',
-  secretAccessKey: '',
-  region: 'ca-central-1',
-});
-
-export default AWS;
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.finder.app",
-  appName: "Book Finder",
-  webDir: "out",
-};
-
-export default config;
