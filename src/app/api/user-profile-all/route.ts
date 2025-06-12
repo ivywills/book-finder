bb8a0c010311aa40cd06bd3addfdd873250b7767
Ivy Wills <ivy.wills@mail.utoronto.ca>
2025-06-11 22:10:21 -0400

    fix: build error

--- src/app/api/user-profile-all/route.ts
+++ src/app/api/user-profile-all/route.ts
@@ -1,9 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     await client.connect();
     const db = client.db('bookfinder');
